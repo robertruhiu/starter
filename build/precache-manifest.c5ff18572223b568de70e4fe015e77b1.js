a36822d6427f2c05450083cbca8ff821e94106f8
robertruhiu <robertruhiu@gmail.com>
2020-06-14 11:19:00 +0000

    first

---
+++ build/precache-manifest.c5ff18572223b568de70e4fe015e77b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc05d71e474bcdf7679c8cec983d90e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f53ac70d02bae24e499",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "96d6979746bb877f9718",
+    "url": "/static/js/2.2f6e4fc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2f6e4fc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f53ac70d02bae24e499",
+    "url": "/static/js/main.cbed25c4.chunk.js"
+  },
+  {
+    "revision": "1c347c79d652a4caddf3",
+    "url": "/static/js/runtime-main.1e250d2a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
